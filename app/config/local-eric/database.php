3b16cc520fb936f6e8abfe0f229d9eed2b578a94
Eric Tendian <erictendian@gmail.com>
2014-05-24 23:11:35 -0500

    Added config for local development

--- app/config/local-eric/database.php
+++ app/config/local-eric/database.php
@@ -57,17 +57,6 @@ return array(
 			'prefix'    => '',
 		),
 
-		'legacy' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
 	),
 
 	/*
